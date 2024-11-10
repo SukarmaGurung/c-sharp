cc32648690a2f6c5f74add15f44e8faa57dc92b1
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-10 08:24:16 +0545

    done for the day

--- Fifth Semester/Program.cs
+++ Fifth Semester/Program.cs
@@ -405,10 +405,16 @@ public class TourPackage {
         pokharaPackage.DisplayPackageDetails();
 
         TourPackage ktmPackage = new TourPackage();
-        ktmPackage.SetUpPackage("KTM", 7, 9000);
+        ktmPackage.SetUpPackage("KTM", 6, 9000);
         ktmPackage.DisplayPackageDetails();
         ktmPackage.BookPackage("Kavita", true);
         ktmPackage.DisplayPackageDetails();
+
+        TourPackage ktmPackage2 = new TourPackage();
+        ktmPackage2.SetUpPackage("KTM", 7, 9000);
+        ktmPackage2.DisplayPackageDetails();
+        ktmPackage2.BookPackage("Ram", true);
+        ktmPackage2.DisplayPackageDetails();
 
     }
 
