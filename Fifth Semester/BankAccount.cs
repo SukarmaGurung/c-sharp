4fa99c3d8c923291182c18ab59db96a7b7b2ee99
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-13 09:59:25 +0545

    banck account created

--- Fifth Semester/BankAccount.cs
+++ Fifth Semester/BankAccount.cs
@@ -7,12 +7,14 @@ namespace Fifth_Semester
 		private string _accountHolderName;
 		private string _bankName;
 		private int _balance;
+		
 
 		public void OpenAccount(string num,string name,string bank ) {
 			_accountNumber = num;
 			_accountHolderName = name;
 			_bankName = bank;
 			_balance = 0;
+			Console.WriteLine("Account Created! Holder Name:" + _accountHolderName);
 
 
         }
