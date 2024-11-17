0b8db785fb57d1c3db9dae735ab4f93343f5d30b
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-17 08:04:48 +0545

    Added Week3Tutorial

---
+++ Week3Tutorial/PartTimeEmployee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Week3Tutorial
+{
+	public class PartTimeEmployee : Employee
+	{
+		public int HourlyRate { get; set; }
+	
+	
+		public PartTimeEmployee( string name, string position, int hourlyRate): base (name,position)
+		{
+		HourlyRate = hourlyRate;
+		}
+	public override int CalculateSalary() {
+		Console.WriteLine("Calculating salary of" + Name);
+		Console.WriteLine("Enter total hours:");
+			int hoursWorked = Convert.ToInt32(Console.ReadLine());
+			return CalculateSalary(hoursWorked);
+	}
+		public int CalculateSalary(int hoursWorked)
+		{
+			return hoursWorked = HourlyRate;
+		}
+        public override void DisplayEmployeeDetails()
+        {
+            base.DisplayEmployeeDetails();
+			Console.WriteLine("part time work rate:" + HourlyRate);
+			Console.WriteLine("---------------------");
+        }
+
+    }
+}
+
