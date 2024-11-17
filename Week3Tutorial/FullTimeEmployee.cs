a6eafa1456cb6090744d3f9bd93b27ad0081d207
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-17 07:31:39 +0545

    Week 3

---
+++ Week3Tutorial/FullTimeEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Week3Tutorial
+{
+	public class FullTimeEmployee : Employee
+	{
+		public int FixedSalary { get; set; }
+		public FullTimeEmployee( string name,string position,int fixedSalary):base(name,position)
+		{
+			FixedSalary = fixedSalary;
+
+		}
+        public override int CalculateSalary()
+        {
+			return FixedSalary;
+        }
+        public override void DisplayEmployeeDetails()
+        {
+            base.DisplayEmployeeDetails();
+            Console.WriteLine("fixed salary:" + FixedSalary);
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
+
