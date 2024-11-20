1d8c66e8599a781185cd9bf426a0e7f046022e22
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-20 09:52:46 +0545

    week 3 lab

--- Week3Tutorial/Area.cs
+++ Week3Tutorial/Area.cs
@@ -6,20 +6,20 @@ namespace Week3Tutorial
 		public void CalculateArea(int length)
 		{
 			int area = length * length;
-			Console.WriteLine("The area for square with length" + ""+length + "is:" + area);
+			Console.WriteLine("The area for square with length " + " "+length + " is: " + area);
 		}
         //Create an overload method to calculate area of circle for a given
         //Create an overload method for calculating area of rectangle
         public void CalculateArea(double radius)
 		{
 			double area = 3.14 * radius * radius;
-            Console.WriteLine("The area for circle with radius" + ""+ radius + "is:" + area);
+            Console.WriteLine("The area for circle with radius " + " "+ radius + " is: " + area);
 
         }
         public void CalculateArea(int length, int breadth)
         {
             int area = length * breadth;
-            Console.WriteLine("The area for reactangle with length" + "" + length + " and breadth"+ ""+ breadth + "is:" + area);
+            Console.WriteLine("The area for reactangle with length " + " " + length + " and breadth "+ " "+ breadth + " "+"is: " + area);
         }
         public static void Main()
         {
