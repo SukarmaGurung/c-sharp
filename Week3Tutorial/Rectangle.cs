6fc50602e0a99af3f57e11a7c6544052b2a449ba
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-20 09:02:35 +0545

    week 3 lab

--- Week3Tutorial/Rectangle.cs
+++ Week3Tutorial/Rectangle.cs
@@ -1,11 +1,41 @@
 ﻿using System;
-namespace Week3Tutorial
-{
-	public class Rectangle
-	{
-		public Rectangle()
-		{
-		}
-	}
-}
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week3Tutorial;
 
+namespace Week3
+{
+    public class Rectangle : Shape
+    {
+        public float Length { get; set; }
+        public float Breadth { get; set; }
+        public float Area { get; set; }
+        public float Perimeter { get; set; }
+        public Rectangle(float length, float breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+
+        }
+        public override float CalculateArea()
+        {
+            Area = Length * Breadth;
+            return Area;
+        }
+
+        public override float CalculatePerimeter()
+        {
+            Perimeter = 2 * (Length + Breadth);
+            return Perimeter;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Area = " + Area);
+            Console.WriteLine("Perimeter = " + Perimeter);
+        }
+    }
+}
