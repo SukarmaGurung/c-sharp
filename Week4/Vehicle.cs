bb24bb295d4f5d03268f1317bccd895ac5c97d36
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-24 08:09:01 +0545

    vehicle

---
+++ Week4/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Week4
+{
+	struct Vehicle: IDrivable {
+		public string Name { get; }
+		public string VehicleType { get; }
+
+
+
+        public Vehicle(string name, string vehicleType)
+        {
+            Name = name;
+            VehicleType = vehicleType;
+        }
+
+        #region
+        public void Drive()
+        {
+            Console.WriteLine($"{Name}, a {VehicleType}, is driving on the track!");
+
+        }
+        #endregion
+    }
+}
+
+
