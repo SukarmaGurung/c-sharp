0068008ff2ce400b25ac734f1ca7b75d224ac480
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-27 09:17:54 +0545

    week 4 lab

--- Week4/Program.cs
+++ Week4/Program.cs
@@ -1,19 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 namespace Week4
 {
     public class Program
-{
-    public static void Main()
     {
-       Laptop dell = new Laptop("Dell");
-        dell.DisplayDetails();
-        Laptop lenovo = new Laptop("lenovo");
-        dell.DisplayDetails();
-        lenovo.DisplayDetails();
+        public static void Main()
+        {
+            //Laptop dell = new Laptop("Dell");
+            // dell.DisplayDetails();
+            // Laptop lenovo = new Laptop("lenovo");
+            // dell.DisplayDetails();
+            // lenovo.DisplayDetails();
+              
+            Vehicle vehicle = new Vehicle("Ferrari", Duplicate.Vehicle.Car);
+            vehicle.Drive();  //--------------
+                              //vehicle.PlayRadio();
+
+            //IDrivable vehicleI = new IDrivable();
+            IDrivable vehicleI = vehicle;
+            vehicleI.PlayRadio();
+            vehicleI.Drive(); //--------------
 
 
+
+
+        }
     }
 }
-}
-
-
