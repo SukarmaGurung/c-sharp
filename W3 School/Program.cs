1d4fb85bebc04531a413b84d219b30580d548c65
SukarmaGurung <sukarmagurung20@gmail.com>
2024-11-29 19:16:34 +0545

    Initial commit for C# project

---
+++ W3 School/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+//It is an object-oriented programming language created by Microsoft that runs on the .NET Framework.
+//C# has roots from the C family, and the language is close to other popular languages like C++ and Java.
+
+
+using System; //using System means that we can use classes from the System namespace.
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!");
+            Console.WriteLine(3 + 3);
+        }
+    }
+}
+
