dd286618d36ad6a6d4a41e668860c6eb7b598046
SukarmaGurung <sukarmagurung20@gmail.com>
2024-12-01 07:49:35 +0545

    json

--- Week5/Program.cs
+++ Week5/Program.cs
@@ -1,4 +1,5 @@
-﻿using Week5;
+﻿using System.Text.Json;
+using Week5;
 
 namespace Week5
 {
@@ -8,7 +9,7 @@ namespace Week5
         {
             Student studentData = new Student()
             {
-                ID = Guid.NewGuid().ToString(),
+                ID = Guid.NewGuid().ToString(), //unique value 
                 Name = "Sukarma",
                 Age = 20,
                 Marks = 67,
@@ -23,8 +24,38 @@ namespace Week5
             Console.WriteLine($"ID: {studentData.ID}");
             Console.WriteLine($"Name: {studentData.Name}");
             Console.WriteLine($"Age: {studentData.Age}");
+            Console.WriteLine($"AcademicYear: {studentData.AcademicYear}");
+            Console.WriteLine($"Marks: {studentData.Marks}");
 
         }
+
+        public static void AddDataToJSONFile(Student data, string fileName)
+        {
+            string jsonString = JsonSerializer.Serialize(data);
+            File.WriteAllText(fileName, jsonString);
+            Console.WriteLine("Data saved to file.");
+        }
+
+        public static Student ReadDataFromJSONFile(string fileName)
+        {
+            string jsonFromFile = File.ReadAllText(fileName);
+            Student deserializedStudentData = JsonSerializer.Deserialize<Student>(jsonFromFile);
+
+            return deserializedStudentData;
+        }
+        //public static void AddDataToJSONFile(Student data, string fileName)
+        //{
+        //    string jsonString = JsonSerializer.Serialize(data);
+        //    File.WriteAllText(fileName, jsonString);
+        //    Console.WriteLine("Data saved to file.");
+
+        //}
+        //public static Student ReadDataFromJSONFile(string fileName)
+        //{
+        //    string jsonFromFile = File.ReadAllText(fileName);
+        //    Student deserializedStudentData = JsonSerializer.Deserialize<Student>(jsonFromFile);
+        //    return deserializedStudentData
+        //}
     }
 }
 
