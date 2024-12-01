4f14c4b7e00aac333be058bba298f70e10f64d2b
SukarmaGurung <sukarmagurung20@gmail.com>
2024-12-01 07:35:22 +0545

    guid

--- Week5/Program.cs
+++ Week5/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Week5;
+
+namespace Week5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Student studentData = new Student()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Name = "Sukarma",
+                Age = 20,
+                Marks = 67,
+                AcademicYear = "3rd"
+            };
+            DisplayDetails(studentData);
+
+        }
+        public static void DisplayDetails(Student studentData)
+        {
+            Console.WriteLine("Student data: ");
+            Console.WriteLine($"ID: {studentData.ID}");
+            Console.WriteLine($"Name: {studentData.Name}");
+            Console.WriteLine($"Age: {studentData.Age}");
+
+        }
+    }
+}
+
 
--- Week5/Student.cs
+++ Week5/Student.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿
 namespace Week5
 {
-	public class Student
-	{
-		public Student()
-		{
-		}
-	}
-}
+    public class Student
+    {
+        public string ID;
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string AcademicYear { get; set; }
+        public int Marks { get; set; }
 
+
+        //public static void DisplayDetails(Student studentData)
+        //{
+        //    Console.WriteLine("Student Data:");
+        //    Console.WriteLine($"ID:{studentData.ID}");
+
+
+        //}
+    }
+}
