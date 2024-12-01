c0b8fc7bd89ef76edc8f877e16629aeca6c98150
SukarmaGurung <sukarmagurung20@gmail.com>
2024-12-01 08:11:11 +0545

    bin ko vtra show in finder

--- Week5/Student.cs
+++ Week5/Student.cs
@@ -3,7 +3,7 @@ namespace Week5
 {
     public class Student
     {
-        public string ID;
+        public string ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public string AcademicYear { get; set; }
